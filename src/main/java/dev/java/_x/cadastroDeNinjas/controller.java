b4bc481f30dc0ee17a894391282874404d94b749
DanielMouraoDev <danielmourao777@gmail.com>
2025-09-29 14:57:42 -0300

    corrigindo a versao do java

---
+++ src/main/java/dev/java/_x/cadastroDeNinjas/controller.java
@@ -0,0 +1,16 @@
+package dev.java._x.cadastroDeNinjas;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class controller {
+
+    @GetMapping("/Boasvindas")
+    public String Boasvindas() {
+        return "Essa é a minha primeira menssagem nessa rota";
+    }
+}
