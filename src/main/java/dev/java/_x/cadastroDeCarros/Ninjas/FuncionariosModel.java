c6f9d869e049bb7984d59d89d04ab14c5b643342
DanielMouraoDev <danielmourao777@gmail.com>
2025-10-01 16:22:40 -0300

    Feat: Coloca o Lombok na aplicação e insere anotaçoes de Data,  Noargs e allargs constructors

--- src/main/java/dev/java/_x/cadastroDeCarros/Ninjas/FuncionariosModel.java
+++ src/main/java/dev/java/_x/cadastroDeCarros/Ninjas/FuncionariosModel.java
@@ -2,11 +2,16 @@ package dev.java._x.cadastroDeCarros.Ninjas;
 
 import dev.java._x.cadastroDeCarros.Missoes.TarefasModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class FuncionariosModel {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,49 +25,4 @@ public class FuncionariosModel {
     private TarefasModel tarefas;
 
 
-    public FuncionariosModel() {
     }
-
-    public FuncionariosModel(Long id, String nome, String email, int idade) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-
-        }
