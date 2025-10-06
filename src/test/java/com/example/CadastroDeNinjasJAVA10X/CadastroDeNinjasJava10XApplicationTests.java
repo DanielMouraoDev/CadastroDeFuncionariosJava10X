2cdc81b76a167b050e31d2174f717c850e9a0cbc
DanielMouraoDev <danielmourao777@gmail.com>
2025-10-06 14:04:35 -0300

    Teste 2

---
+++ src/test/java/com/example/CadastroDeNinjasJAVA10X/CadastroDeNinjasJava10XApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CadastroDeNinjasJAVA10X;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasJava10XApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
