dc25ae4bd00c7304c72a2ad085cc2a282ca489b4
DanielMouraoDev <danielmourao777@gmail.com>
2025-10-08 19:15:53 -0300

    Feat: Cria as rotas do controller com mock

--- src/main/java/dev/java/_x/cadastroDeFuncionarios/Tarefas/TarefasController.java
+++ src/main/java/dev/java/_x/cadastroDeFuncionarios/Tarefas/TarefasController.java
@@ -1,4 +1,28 @@
 package dev.java._x.cadastroDeFuncionarios.Tarefas;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("tarefas/criar")
 public class TarefasController {
+
+@PostMapping("/criar")
+    public String criarTarefas() {
+    return "Tarefa criada com seucesso";
+}
+
+@PutMapping("/alterar")
+    public String alterarTarefa() {
+    return "Tarefa Alterada";
+}
+
+@DeleteMapping("/deletar")
+    public String deletarTarefa() {
+    return "Tarefa deletada";
+}
+
+@GetMapping("/listar")
+    public String listar() {
+    return "Tarefas listadas com sucesso";
+}
 }
