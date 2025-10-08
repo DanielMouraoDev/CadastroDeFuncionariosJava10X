fca42ae42ccf0d4746eedb3dc2bb8b6880b1e8d3
DanielMouraoDev <danielmourao777@gmail.com>
2025-10-08 18:55:04 -0300

    Atualiza o controller com mock de dados

--- src/main/java/dev/java/_x/cadastroDeFuncionarios/Funcionarios/FuncionariosController.java
+++ src/main/java/dev/java/_x/cadastroDeFuncionarios/Funcionarios/FuncionariosController.java
@@ -1,16 +1,44 @@
 package dev.java._x.cadastroDeFuncionarios.Funcionarios;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class FuncionariosController {
 
-    @GetMapping("localhost:8080/Boasvindas")
+    @GetMapping("/Boasvindas")
     public String Boasvindas() {
         return "Essa é a minha primeira menssagem nessa rota";
+    }
+
+    // CREATE
+    @PostMapping("/criar")
+    public String criarfuncionario() {
+        return "Funcionario criado";
+    }
+
+    @GetMapping("/todos")
+    public String MostrarTodososFuncionarios() {
+        return "Mostrar Funcionario ";
+    }
+
+    //CREATE POR ID
+    @GetMapping("/todosID")
+    public String MostrarTodososFuncionariosporID() {
+        return "Mostrar Funcionario por ID";
+    }
+
+    //UPDATE
+    @PutMapping("/AlterarID")
+    public String AlterarFuncionariosporID() {
+        return "Alterar FuncionarioporID";
+    }
+
+    //DELETE
+    @DeleteMapping("/DeletarID")
+    public String DeletarFuncionariosporID() {
+        return "Funcionario Deletado porID";
+
     }
 }
--- src/main/resources/Db/migrations/V2__Add_rank_tb_cadastro.sql
+++ src/main/resources/Db/migrations/V2__Add_rank_tb_cadastro.sql
@@ -1,4 +1,4 @@
--- V2: Migrations para adicionar a coluna de ranking na tabela de cadastros
+-- V2: Migrations para adicionar a coluna de profissão na tabela de cadastros
 
 ALTER TABLE tb_cadastro
 ADD COLUMN profissão VARCHAR (255);
